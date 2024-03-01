0d8b9c4f557d612f7ed66926839b07989f65dcf5
Emrullah AKAN <akanemrullah@gmail.com>
2024-03-01 11:06:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[][] stars = new String[7][4];
+
+        for (int i = 0; i < stars.length; i++) {
+            for (int j = 0; j < stars[i].length; j++) {
+
+                if (i == 0 || i == 3 || i == 6) {
+                    stars[i][j] = " * ";
+                } else if (j == 0 || j == 3) {
+                    stars[i][j] = " * ";
+                } else {
+                    stars[i][j] = "   ";
+                }
+            }
+        }
+        for (int i = 0; i < stars.length; i++) {
+            for (int j = 0; j < stars[i].length; j++) {
+
+                System.out.print(stars[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+}
